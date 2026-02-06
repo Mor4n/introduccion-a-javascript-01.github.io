23fdd1077ee452339dd279b47f5c423d4a300c18
Mor4n <anm0ran@outlook.com>
2026-02-05 20:02:28 -0700

    Añadido el console log para imprimir los libros en el ciclo for y añadidas las llamadas a las funciones

--- 04-funciones/libros.js
+++ 04-funciones/libros.js
@@ -1,4 +1,5 @@
 
+// Declaración de variables y funciones
 
 // Arreglo inicial para guardar los libros
 let librosLeidos = [];
@@ -6,6 +7,8 @@ let librosLeidos = [];
 // Función tipo Arrow para agregar un libro
 
 const agregarLibro = (titulo) =>{
+    
+    // Método push del arreglo para añadir el título
     librosLeidos.push(titulo);
 }
 
@@ -15,7 +18,21 @@ const mostrarLibrosLeido = () =>{
     // Ciclo for para recorrer todos los libros y mostrarlos
     for (let i = 0; i < librosLeidos.length; i++) { 
         
+        // Se imprime el valor del libro en la posición del array 
+        console.log(`Libro ${i} - ${librosLeidos[i]}`);
         
     }
     
 }
+
+// Llamadas de funciones
+
+// Funciones de agregar libro
+agregarLibro("Solanin");
+agregarLibro("Tokyo Ghoul");
+agregarLibro("I am a hero");
+agregarLibro("Chainsaw Man");
+agregarLibro("Gintama");
+
+// Función para mostrar los libros
+mostrarLibrosLeido();
