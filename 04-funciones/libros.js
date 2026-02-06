3b9a3e418f8b08a30dfc7b70c61893a6cd54b757
Mor4n <anm0ran@outlook.com>
2026-02-05 19:52:12 -0700

    Añadidos comentarios explicando más o menillos cada cosa, añadido el push para agregar el título al arreglo de los libros como vimos en la clase anterior, me falta recorrer los libros leídos

--- 04-funciones/libros.js
+++ 04-funciones/libros.js
@@ -1,11 +1,21 @@
 
 
+// Arreglo inicial para guardar los libros
 let librosLeidos = [];
 
-const agregarLibro = (titulo) =>{
+// Función tipo Arrow para agregar un libro
 
+const agregarLibro = (titulo) =>{
+    librosLeidos.push(titulo);
 }
 
+// Función tipo Arrow para mostrar los libros
 const mostrarLibrosLeido = () =>{
 
+    // Ciclo for para recorrer todos los libros y mostrarlos
+    for (let i = 0; i < librosLeidos.length; i++) { 
+        
+        
+    }
+    
 }
