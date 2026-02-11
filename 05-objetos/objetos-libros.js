334ec52b189a86536c802b3c6f53723fd78d2d91
Mor4n <anm0ran@outlook.com>
2026-02-10 20:32:53 -0700

    Añadido la parte opcional de propiedad que contenga los capitulos del libro

--- 05-objetos/objetos-libros.js
+++ 05-objetos/objetos-libros.js
@@ -6,9 +6,16 @@ const libro = {
     autor: "Inio Asano",
     año:2005,
     estado:"disponible",
+    capitulos:[],
     describirLibro(){
         console.log(`Nombre del libro: ${this.titulo}, escrito por ${this.autor}, lanzado el ${this.año}. El estado de este libro es: ${this.estado}.`);
     },
+    agregarCapitulo(capitulo){
+        this.capitulos.push(capitulo); //Añado con el push que vimos, el capitulo que hayamos pasado al llamar al método
+    },
+    eliminarCapitulo(){
+        this.capitulos.pop(); //Borro el último capítulo con pop
+    }
 }
 
 // Objeto libro2
@@ -17,10 +24,42 @@ const libro2 = {
     autor: "Takehiko Inoue",
     año:1998,
     estado:"prestado", 
+    capitulos:["01 - Agradecimientos","02 - Introducción a Vagabond","03 - El inicio de la historia"],
     describirLibro(){
         console.log(`Nombre del libro: ${this.titulo}, escrito por ${this.autor}, lanzado el ${this.año}. El estado de este libro es: ${this.estado}.`);
+    },
+    agregarCapitulo(capitulo){
+        this.capitulos.push(capitulo); 
+    },
+    eliminarCapitulo(){
+        this.capitulos.pop();
     }
 }
 
+// Llamada al método describir libro para mostrar los datos del libro en la consola
 libro.describirLibro();
 libro2.describirLibro();
+
+// Llamada al método agregar capitulo del objeto libro para agregar un nuevo capitulo al array del objeto
+libro.agregarCapitulo("01 - Introducción"); 
+libro.agregarCapitulo("02 - El inicio de la historia"); 
+libro.agregarCapitulo("03 - Un nuevo día"); 
+
+console.log(`Los capítulos del libro 1 son: ${libro.capitulos}`);
+
+// Llamada al método eliminar capitulo, para borrar los últimos capítulos agregados
+
+libro.eliminarCapitulo(); 
+libro.eliminarCapitulo();
+
+console.log(`Los capítulos del libro 1 son: ${libro.capitulos}`); // 01 - Introducción
+
+libro2.agregarCapitulo("04 - Protagonista nuevo"); // Se agrega el capítulo 04
+
+console.log(`Los capítulos del libro 2 son: ${libro2.capitulos}`); // 01 - Agradecimientos,02 - Introducción a Vagabond,03 - El inicio de la historia,04 - Protagonista nuevo
+
+libro2.eliminarCapitulo(); // Se elimina el último capítulo agregado
+
+console.log(`Los capítulos del libro 2 son: ${libro2.capitulos}`); // 01 - Agradecimientos,02 - Introducción a Vagabond,03 - El inicio de la historia
+
+
