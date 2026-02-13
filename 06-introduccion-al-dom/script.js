daad9aa370844478d568fac33a341b849cdba894
Mor4n <anm0ran@outlook.com>
2026-02-12 21:32:40 -0700

    intento de lo que aparece en la doc de mozilla de formulario pq haciendo lo del botón que vimos no m agarró para esto unu

--- 06-introduccion-al-dom/script.js
+++ 06-introduccion-al-dom/script.js
@@ -1,6 +1,18 @@
 
 // variables iniciales
-const formulario = document.querySelector("#formulario");
-const nombre = document.querySelector("#nombre");
-const comentario = document.querySelector("#comentario");
-const boton_enviar = document.querySelector("#boton_enviar");
+const formulario = document.querySelector("#formulario"); // formulario general
+const nombre = document.querySelector("#nombre"); // input de nombre
+const comentario = document.querySelector("#comentario"); // textarea de comentario
+const boton_enviar = document.querySelector("#boton_enviar"); // boton de enviar
+
+
+
+
+function enviarFormulario (event){
+    event.preventDefault(); 
+    console.log(`${nombre.value} - ${comentario.value}`);
+
+
+}
+
+formulario.addEventListener("submit",enviarFormulario);
