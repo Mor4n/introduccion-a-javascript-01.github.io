702e3471f9a51083a7d4b473abc73faadfa659e7
Mor4n <anm0ran@outlook.com>
2026-02-12 21:20:01 -0700

    Mejor deje solo el nombre y comentario, añadidos variables iniciales en el script

---
+++ 06-introduccion-al-dom/script.js
@@ -0,0 +1,6 @@
+
+// variables iniciales
+const formulario = document.querySelector("#formulario");
+const nombre = document.querySelector("#nombre");
+const comentario = document.querySelector("#comentario");
+const boton_enviar = document.querySelector("#boton_enviar");
