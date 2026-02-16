b52da52a99a87945512fa20d51948afaf00f10b0
Mor4n <anm0ran@outlook.com>
2026-02-15 20:22:11 -0700

    Añadida fecha y hora, README y captura, le voy a poner foto de perfil mas adelante, añadidos mas comentarios

--- 06-introduccion-al-dom/README.md
+++ 06-introduccion-al-dom/README.md
@@ -1,17 +1,25 @@
-# Lección 6 - Introducción al DOM
+# Lección 6 - Introducción al DOM: Caja de Comentarios (Simple Comment Box)
 
 
 
 
 ## Archivos del proyecto
 
-- **index.html**: Archivo HTML para probar el código en el navegador
-- **objetos-libros.js**: Ejercicio realizado para la lección 5: Objeto a partir de un libro
-- **retos.js**: Retos vistos en clase
-- **README.md**: Documentación y explicación de la lección 5
+- **index.html**: Archivo HTML donde está la base de la práctica de la lección 6: Caja de Comentarios (Simple Comment Box)
+- **script.js**: Archivo de Javascript con la funcionalidad para realizar la caja de comentarios
+- **style.cs**: Archivo de CSS con el diseño del index.html
+- **README.md**: Documentación y explicación de la lección 6
 
 - **ejemplos-clase**: Carpeta con notas y ejemplos de vistos en clase sobre
-- **ejemplos-clase/.js**: Notas y ejemplos acerca del DOM
+- **ejemplos-clase/index.html**: Archivo base para poder ejecutar el script.js
+- **ejemplos-clase/script.js**: Notas acerca del DOM en javascript vistos en clase
+
+- **hola_dom**: Carpeta con notas del ejemplo de "Hola usuario" con el DOM y javascript
+- **hola_dom/index.html**: Archivo HTML base para ejecutar el script.js
+- **hola_dom/script.js**: Archivo de Javascript con la funcionalidad para modificar el DOM
+
+- **ejemplos-clase/index.html**: Archivo base para poder ejecutar el script.js
+- **ejemplos-clase/script.js**: Notas acerca del DOM en javascript vistos en clase
 
 - **img/Captura.png**: Captura de pantalla del funcionamiento del programa en el navegador
 
@@ -31,16 +39,16 @@ A continuación se muestra una captura de pantalla del código funcionando en la
 
 Abra el archivo 
 ```index.html```
-en su navegador y revise la consola para ver los ejemplos y resultados
+en su navegador y revise el sitio web para probar la funcionalidad del mismo
 
 También puede mirar el código de JavaScript abriendo el archivo
-```.js```
+```script.js```
 dentro de su editor de código preferido o dentro de Github.
 
 ## Despliegue
 
 Se desplegó en Github Pages a partir de este repositorio, puedes ver la página a través del siguiente link:
-
+https://mor4n.github.io/introduccion-a-javascript-01.github.io/06-introduccion-al-dom/index.html
 
 
 
@@ -51,3 +59,4 @@ Se desplegó en Github Pages a partir de este repositorio, puedes ver la página
 https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
 https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
 https://developer.mozilla.org/en-US/docs/Web/API/Element/remove
+https://stackoverflow.com/questions/4744299/how-to-get-datetime-in-javascript
--- 06-introduccion-al-dom/script.js
+++ 06-introduccion-al-dom/script.js
@@ -12,7 +12,7 @@ function enviarFormulario (event){
     event.preventDefault(); 
     // console.log(`${nombre.value} - ${comentario.value}`);
 
-    // creo un div como contenedor del comentario, este guardará tanto el texto del comentario, como el botón de eliminar
+    // creo un div como contenedor del comentario, este guardará TODO el comentario en sí
     const nuevoComentario = document.createElement("div");
     // le agrego la clase contenedor-comentario para editarle el estilo
     nuevoComentario.setAttribute("class","contenedor-comentario");
@@ -20,26 +20,41 @@ function enviarFormulario (event){
     // le agrego el id al comentario
     nuevoComentario.setAttribute("id",idComentario);
 
-    // creo un div texto comentario, aqui es donde voy a guardar el usuario y el contenido del comentario
+    // creo un div contenido comentario, aqui es donde voy a guardar el contenido como: usuario, fecha y el texto del comentario
     const textoComentario = document.createElement("div");
-    textoComentario.setAttribute("class","texto-comentario");
+    textoComentario.setAttribute("class","contenido-comentario");
+
+    // creo un div usuario-fecha, donde guardaré el nombre de usuario y fecha
+    const usuarioFechayHora = document.createElement("div");
+    usuarioFechayHora.setAttribute("class","usuario-fecha");
 
     // creo el nombre del usuario
     const nombreComentario = document.createElement("h3");
     nombreComentario.setAttribute("class","nombre-usuario");
-    nombreComentario.textContent = `@${nombre.value}`;
+    nombreComentario.textContent = `@${nombre.value || "Usuario sin nombre" }`;
 
-    //creo un parrafo con el comentario escrito
+    // creo la fecha y hora
+    const fechaHora = document.createElement("p");
+    fechaHora.setAttribute("class","fecha-hora");
+    fechaHora.textContent = new Date().toLocaleString();
+
+    // anexo el usuario y la fecha a su contenedor usuarioFechayHora
+
+    usuarioFechayHora.appendChild(nombreComentario);
+    usuarioFechayHora.appendChild(fechaHora);
+
+
+    //creo un parrafo con el contenido del comentario escrito
     const contenidoComentario = document.createElement("p");
-    contenidoComentario.setAttribute("class","texto-comentario");
+    contenidoComentario.setAttribute("class","contenido-comentario");
     // agrego el texto del input al parrafo recien creado de comentario
     contenidoComentario.textContent = `${comentario.value}`;
 
-    // agrego el usuario y el contenido del comentario al contenedor del comentario
-    textoComentario.appendChild(nombreComentario);
+    // agrego el usuario-fechayhora y el contenido del comentario al contenedor del comentario, asi el usuario y fecha y hora los puedo estilizar de forma horizontal arriba y que el comentario posteado quede abajo
+    textoComentario.appendChild(usuarioFechayHora);
     textoComentario.appendChild(contenidoComentario);
 
-    // y agrego todos estos al contenedor general del comentario,el cual incluirá el botón de eliminar en unos momentos
+    // y agrego todos estos al contenedor general del comentario,el cual incluirá el botón de eliminar en unos momentos, entonces será a la izquierda el usuario fecha y hora, teniendo abajo el comentario y a la derecha el botón de eliminar
     nuevoComentario.appendChild(textoComentario);
 
 
