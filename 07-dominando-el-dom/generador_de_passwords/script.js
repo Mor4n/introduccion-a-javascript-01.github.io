fe4d00c9c4a9e8a53a2d8bf1b41e7f430fdad3a2
Mor4n <anm0ran@outlook.com>
2026-02-17 18:38:06 -0700

    Ideas de que puedo hacer para generar la contraseña aleatoria de comentarios en js

---
+++ 07-dominando-el-dom/generador_de_passwords/script.js
@@ -0,0 +1,12 @@
+/* hacer un abecedario? tipo, array de abecedario? array de minusculas y mayusculas y simbolos y numeros?
+y de que hacer un random(), que el random sea una posiicon entre 0 y el tamaño del arreglo total?
+y esto agregarlo en un arreglo letra por letra y juntarlo al final?
+*/
+
+
+const generate_btn = document.querySelector("#generate_btn");
+
+
+generate_btn.addEventListener( () => {
+    
+})
