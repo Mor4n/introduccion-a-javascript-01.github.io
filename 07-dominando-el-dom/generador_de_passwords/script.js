27df0657614f5192bffb7656a74a30de41fa0e8c
Mor4n <anm0ran@outlook.com>
2026-02-19 22:34:15 -0700

    Añadido tamaño de arreglo fijo en base al valor de length

--- 07-dominando-el-dom/generador_de_passwords/script.js
+++ 07-dominando-el-dom/generador_de_passwords/script.js
@@ -19,7 +19,6 @@ const numbers = ['0','1','2','3','4','5','6','7','8','9'];
 const symbols = ['!','#','$','%','&'];
 
 
-
 generate_btn.addEventListener("click", () => {
     // console.log("Presionaste este boton");
 
@@ -33,14 +32,40 @@ generate_btn.addEventListener("click", () => {
     generated_password.innerHTML = result;
 });
 
+/**
+ * miniidea que tengo:
+ * Tener un arreglo, donde tendrá de limite de tamaño el rango, en un for, verifico de forma random cada posicion del arreglo, 
+ * si tiene un valor añadido, paso
+ * si no tiene, verifico si ya fue añadido al menos 1 vez ya sea uppercase, lowercase, number, symbols, de no ser añadido, agarro uno de estos de forma aleatoria y lo añado, si ya todos fueron una vez, lo vuelvo a poner pero en otro lugar
+ * O sea xd tipo: posicion random inicial para poner un valor en el arreglo, si tiene un dato, paso a otra posicion random, si no tiene un valor, verifico cual de las 4 opciones ya fue añadida (ya sea uppercase, lowercase, number, symbols) y añado una de estas de forma aleatoria, si ya todas fueron añadidas, vuelvo a añadir una de estas
+ */
 
 const generate_password = (length, uppercase,lowercase, number,  symbols) =>{
+
+
     console.log(`Generando una contraseña:
         Con longitud: ${length}, 
         Con mayusculas: ${uppercase},
         Con minusculas: ${lowercase},
         Con numeros: ${number},
         Con simbolos: ${symbols} `);
+
+
+    // Inicializo el arreglo con el tamaño fijo, pero vacio
+    const password_array = new Array(Number(length)); // El tamaño del array será el valor de length, lo convierto a number porque con el input es string y si lo dejo como string, en realidad LO METE en la posición 0 el valor de length, lo que yo requiero es ponerlo de tamaño para mi idea
+    // console.log(password_array);
+    
+    
+    // Si ninguno de estas opciones es true, no puedo hacer la contraseña
+    if(!uppercase && !lowercase && !number && !symbols){
+        return "Por favor, seleccione al menos una opción para brindarle una contraseña";
+    }
+    else{
+
+
+
+    }
+
 
     return "CONTRASEÑA GENERADA";
     
