f0d93d5870c0d864918bf40f438ec519f7b0f4e1
Mor4n <anm0ran@outlook.com>
2026-02-23 01:35:18 -0700

    Reestructurado el html para intentar hacer un miniclon de ig mejor ;w;

---
+++ 08-proyecto-final/script.js
@@ -0,0 +1,11 @@
+
+// Variables del dom
+
+const create_post_button = document.querySelector(".create-post-button");
+const post_form = document.querySelector("#post-form");
+const username = document.querySelector("#username");
+const content = document.querySelector("#content");
+const submit_button = document.querySelector("#submit-btn");
+const posts_container = document.querySelector("#posts-container");
+
+// Para mostrar la creación del post
